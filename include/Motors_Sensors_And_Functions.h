51f2d87b790b8964b7832ecc2b748a36d0ede856
raymond1225 <sfampy418@gmail.com>
2022-08-04 21:45:16 -0400

    Changes for fair.

--- include/Motors_Sensors_And_Functions.h
+++ include/Motors_Sensors_And_Functions.h
@@ -5,19 +5,46 @@
 // 2=back left
 // 3=front right
 // 4=back right
-pros::Motor DT1(1, true);
-pros::Motor DT2(2, true);
-pros::Motor DT3(3);
+pros::Motor DT1(6, true);
+pros::Motor DT2(7, true);
+pros::Motor DT3(5);
 pros::Motor DT4(4);
 // These are the two intake motors
-pros::Motor I1(5);
-pros::Motor I2(6);
+pros::Motor I1(1);
+pros::Motor R1(8);
 // These are our two fly-wheel motors
-pros::Motor FW1(7);
-pros::Motor FW2(8);
+pros::Motor FW1(3);
+pros::Motor FW2(2);
 // These are the net releases on the four corners of our robot.They are in the same order as our drive train.
 pros::ADIDigitalOut Net1(1);
 pros::ADIDigitalOut Net2(2);
 pros::ADIDigitalOut Net3(3);
 pros::ADIDigitalOut Net4(4);
 pros::Controller master(pros::E_CONTROLLER_MASTER);
+
+void Turn (int Direction, int dist){
+  DT1.move_relative((Direction*dist*600)/12.56, 100);
+  DT2.move_relative((Direction*dist*600)/12.56, 100);
+  DT3.move_relative((-Direction*dist*600)/12.56, 100);
+  DT4.move_relative((-Direction*dist*600)/12.56, 100);
+}
+
+void DriveFWD (int dist){
+  DT1.move_relative((dist*600)/12.56, 100);
+  DT2.move_relative((dist*600)/12.56, 100);
+  DT3.move_relative((dist*600)/12.56, 100);
+  DT4.move_relative((dist*600)/12.56, 100);
+}
+
+void IntakeOnOff (int Power){
+  I1.move_velocity(200*Power);
+}
+
+void FlyWheelOnOff (int Power){
+  FW1.move_velocity(135*Power);
+  FW2.move_velocity(135*Power);
+}
+
+void RollerToggle(){
+  R1.move_relative((7.47*900)/12.56, 100);
+};
--- src/main.cpp
+++ src/main.cpp
@@ -24,10 +24,10 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
+//	pros::lcd::initialize();
+//	pros::lcd::set_text(1, "Hello PROS User!");
 
-	pros::lcd::register_btn1_cb(on_center_button);
+//	pros::lcd::register_btn1_cb(on_center_button);
 }
 
 /**
@@ -59,7 +59,33 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous() {
+	FlyWheelOnOff(1);
+ 	pros::delay(5000);
+	IntakeOnOff(1);
+	/**
+	FlyWheelOnOff(1);
+ 	pros::delay(2000);
+	IntakeOnOff(1);
+	pros::delay(2000);
+	IntakeOnOff(0);
+	Turn(1,90);
+	IntakeOnOff(1);
+	DriveFWD(-24);
+	IntakeOnOff(0);
+	Turn(1,45);
+	IntakeOnOff(1);
+	DriveFWD(-12);
+	IntakeOnOff(0);
+	Turn(1,90);
+	IntakeOnOff(1);
+	pros::delay(2000);
+	IntakeOnOff(0);
+	Turn(-1,90);
+	DriveFWD(40);
+	RollerToggle();
+	*/
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -88,6 +114,10 @@ void opcontrol() {
 	DT2.set_brake_mode(MOTOR_BRAKE_HOLD);
 	DT3.set_brake_mode(MOTOR_BRAKE_HOLD);
 	DT4.set_brake_mode(MOTOR_BRAKE_HOLD);
+	R1.set_brake_mode(MOTOR_BRAKE_BRAKE);
+	FW1.set_brake_mode(MOTOR_BRAKE_COAST);
+	FW2.set_brake_mode(MOTOR_BRAKE_COAST);
+	master.clear();
 	while (true) {
 		//JSL and JSR stand for joy-stick left and joy-stick right respectively. They act as a buffer to obtain the raw value of the joysticks of the controller which can go between 0 and 127
 		JSL = master.get_analog(ANALOG_LEFT_Y);
@@ -96,24 +126,9 @@ void opcontrol() {
 		//The reason for the alteration of the joy-stick is that the maximum value I can put into the motor is 100 so scaling the joy-stick to the motor allows for finer movement.
 		//The reason for waiting until the joy-stick reads at least 15 is to avoid unintentional movement if the joy-stick doesn't return directly to center or if the joy-stick flicks back and imputs a negative value before returning to center.
 		//Sometimes when we are driving the joy-stick drifts diagonally a little bit which can lower our total speed so if the joy-stick is above a certian hreshhold I now set it to max speed that way tiny fluctuations in our driving don't effect our proformance.
-		if(15 < JSL && JSL < 100){
-			DT1.move_velocity(JSL*(100/127));
-			DT2.move_velocity(JSL*(100/127));
-		}
-
-		else if(JSL > 100){
-			DT1.move_velocity(100);
-			DT2.move_velocity(100);
-		}
-
-		else if(-15 > JSL && JSL > -100){
-			DT1.move_velocity(JSL*(100/127));
-			DT2.move_velocity(JSL*(100/127));
-		}
-
-		else if(JSL < -100){
-			DT1.move_velocity(-100);
-			DT2.move_velocity(-100);
+		if(abs(JSL) > 5){
+				DT1.move_velocity(JSL);
+				DT2.move_velocity(JSL);
 		}
 
 		else {
@@ -121,26 +136,10 @@ void opcontrol() {
 			DT2.brake();
 		}
 
-		if(15 < JSR && JSR < 100){
-			DT3.move_velocity(JSR*(100/127));
-			DT4.move_velocity(JSR*(100/127));
+		if(abs(JSR) > 5){
+				DT3.move_velocity(JSR);
+				DT4.move_velocity(JSR);
 		}
-
-		else if(JSR > 100){
-			DT3.move_velocity(100);
-			DT4.move_velocity(100);
-		}
-
-		else if(-15 > JSR && JSR > -100){
-			DT3.move_velocity(JSR*(100/127));
-			DT4.move_velocity(JSR*(100/127));
-		}
-
-		else if(JSR < -100){
-			DT3.move_velocity(-100);
-			DT4.move_velocity(-100);
-		}
-
 		//At the beginning I set the motors to the brake mode "hold" so that they would push back any time they were being forced to move. This way our stops are more precise and if we are stuck in a pushing match we cna let the robot do the work and not use more energy than we need.
 		else {
 			DT3.brake();
@@ -162,13 +161,17 @@ void opcontrol() {
 			FWM = 3;
 		}
 
+		if (master.get_digital(DIGITAL_UP)) {
+			FWM = 0;
+		}
+
 		if (FWM == 1) {
-			FW1.move_velocity(100);
-			FW2.move_velocity(100);
+			FW1.move_velocity(135);
+			FW2.move_velocity(135);
 			master.print(0, 0, "Fly-Wheel Mode: %s", "Close");
 			FWAS = (FW1.get_actual_velocity() + FW2.get_actual_velocity())/2;
 			FWP = 100*(FWAS/FW1.get_target_velocity());
-			master.print(1,0, "Fly-Wheel Percentage: %%i", FWP);
+			master.print(1,0, "Fly-Wheel Percentage: %i", FWP);
 			pros::delay(100);
 		}
 
@@ -178,7 +181,15 @@ void opcontrol() {
 			master.print(0, 0, "Fly-Wheel Mode: %s", "Medium");
 			FWAS = (FW1.get_actual_velocity() + FW2.get_actual_velocity())/2;
 			FWP = 100*(FWAS/FW1.get_target_velocity());
-			master.print(1,0, "Fly-Wheel Percentage: %%i", FWP);
+			master.print(1,0, "Fly-Wheel Percentage: %i", FWP);
+			pros::delay(100);
+		}
+
+
+		if (FWM == 0) {
+			FW1.brake();
+			FW2.brake();
+			master.print(0, 0, "Fly-Wheel Mode: %s", "Off");
 			pros::delay(100);
 		}
 
@@ -189,33 +200,41 @@ void opcontrol() {
 			master.print(0, 0, "Fly-Wheel Mode: %s", "Far");
 			FWAS = (FW1.get_actual_velocity() + FW2.get_actual_velocity())/2;
 			FWP = 100*(FWAS/FW1.get_target_velocity());
-			master.print(1,0, "Fly-Wheel Percentage: %%i", FWP);
+			master.print(1,0, "Fly-Wheel Percentage: %i", FWP);
 			pros::delay(100);
 		}
 
 		//This section is fairly simple, if the press the top right bumper the intake pulls in disks from the ground and loads them into the fly-wheel, if the bottom right bumper is pressed it sends disks out the frond of the intake.
 		if (master.get_digital(DIGITAL_R1)) {
 			I1.move_velocity(200);
-			I2.move_velocity(200);
 		}
 
 		else if (master.get_digital(DIGITAL_R2)) {
 			I1.move_velocity(-200);
-			I2.move_velocity(-200);
 		}
 
 		else {
 			I1.move_velocity(0);
-			I2.move_velocity(0);
 		}
 
 		//This section is what I call "Net Mode". When we press the up button on the controller whcih changes the variable NM to 1. While NM equals 1 the drive base is set to hold and the rest of the motors on the drive base are set to 0 and each of the bumpers on the controller are set to activate one of the cylenders to fire the net mechanism. The up button is set to fire them all at once. The X button is set to turn NM to 0 and deactivate net mode.
 		//I made this a seperate mode because if we accidentally slipped and activated the net durin g the match it could easily disqualify us.
-		if (master.get_digital(DIGITAL_UP)) {
+		if (master.get_digital(DIGITAL_X)) {
 			NM = 1;
 			pros::delay(100);
 		}
 
+		if (master.get_digital(DIGITAL_L2)){
+			R1.move_velocity(100);
+		}
+
+		if (master.get_digital(DIGITAL_L1)){
+			R1.move_velocity(-100);
+		}
+
+		else {
+			R1.brake();
+		}
 		while (NM == 1) {
 				DT1.brake();
 				DT2.brake();
@@ -224,7 +243,6 @@ void opcontrol() {
 				FW1.move_velocity(0);
 				FW2.move_velocity(0);
 				I1.move_velocity(0);
-				I2.move_velocity(0);
 
 				if (master.get_digital(DIGITAL_L1)){
 					Net1.set_value(true);
